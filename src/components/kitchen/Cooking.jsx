1b941d685c5f6aee376c7d36039d689f273ab4bb
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-03-16 19:19:51 +0600

    added functionality to remove food item from want to cook table and add to cooking now table

---
+++ src/components/kitchen/Cooking.jsx
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+
+const Cooking = ({cookingNow, totalTime, totalCalories}) => {
+  return (
+    <div>
+      <div className="overflow-x-auto py-1">
+        <table className="table">
+          <thead>
+            <tr>
+              <th></th>
+              <th className="px-0">Name</th>
+              <th className="px-6">Time</th>
+              <th className="px-0">Calories</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cookingNow.map((foodItem, idx) => (
+              <tr key={idx} className="bg-[#28282808] text-c-data">
+                <th>{idx+1}</th>
+                <td className="px-0">{foodItem.recipe_name}</td>
+                <td className="px-6">{foodItem.preparing_time} minutes</td>
+                <td className="px-0">{foodItem.calories} calories</td>
+              </tr>
+            ))}
+            <tr className='text-c-data border-b-0'>
+              <td></td>
+              <td></td>
+              <td className="px-6">Total Time = {totalTime} minutes</td>
+              <td className="pl-0 pr-6">Total Calories = {totalCalories} calories</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+Cooking.propTypes = {
+  cookingNow: PropTypes.array.isRequired,
+  totalTime: PropTypes.number.isRequired,
+  totalCalories: PropTypes.number.isRequired
+};
+
+export default Cooking;
--- src/components/kitchen/ToCook.jsx
+++ src/components/kitchen/ToCook.jsx
@@ -1,7 +1,6 @@
 import PropTypes from "prop-types";
 
-const ToCook = ({ toCook }) => {
-  console.log(toCook);
+const ToCook = ({ toCook, handleAddToCookingList }) => {
   return (
     <div>
       <div className="overflow-x-auto py-1">
@@ -9,21 +8,21 @@ const ToCook = ({ toCook }) => {
           <thead>
             <tr>
               <th></th>
-              <th>Name</th>
-              <th>Time</th>
-              <th>Calories</th>
-              <th>Actions</th>
+              <th className="px-0">Name</th>
+              <th className="px-5">Time</th>
+              <th className="px-0">Calories</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
             {toCook.map((foodItem, idx) => (
               <tr key={idx} className="bg-[#28282808] text-c-data">
-                <th>{idx+1}</th>
-                <td>{foodItem.recipe_name}</td>
-                <td>{foodItem.preparing_time} minutes</td>
-                <td>{foodItem.calories} calories</td>
+                <th className="pr-5">{idx+1}</th>
+                <td className="px-0">{foodItem.recipe_name}</td>
+                <td className="px-5">{foodItem.preparing_time} minutes</td>
+                <td className="px-0">{foodItem.calories} calories</td>
                 <td>
-                  <button className="bg-c-primary btn rounded-3xl">
+                  <button onClick={()=>handleAddToCookingList(foodItem)} className="bg-c-primary py-2 px-3 rounded-3xl text-xs md:text-sm text-black">
                     Preparing
                   </button>
                 </td>
@@ -40,6 +39,7 @@ const ToCook = ({ toCook }) => {
 
 ToCook.propTypes = {
   toCook: PropTypes.array.isRequired,
+  handleAddToCookingList: PropTypes.func
 };
 
 export default ToCook;
