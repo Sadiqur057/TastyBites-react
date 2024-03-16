9184e69ce753ed743341ae9d120b2c65a3c86733
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-03-16 19:24:30 +0600

    configured tailwind for daisy ui and added common classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,16 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'c-header': '#150b2bb3',
+        'c-primary': '#0be58a',
+        'c-secondary': '#878787',
+        'c-data': '#282828b3',
+        'c-seat': '#f7f8f8',
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
