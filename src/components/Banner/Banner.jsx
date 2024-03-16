7072fa0a78d74ceab0c0e44a00b1bf253569494d
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-03-16 19:17:41 +0600

    adjusted button styles in banner section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,13 +8,11 @@ const Banner = () => {
       <div className="w-full  text-center font-lexend rounded-2xl p-6 md:p-14 lg:p-32">
           <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold text-white">Discover an exceptional cooking class tailored for you!</h1>
           <p className="text-sm md:text-base lg:text-lg font-light mt-4 md:mt-6 mb-6 lg:md-10 text-[#ffffffd9]">
-            Learn and Master Basic Programming, Data Structures, Algorithm, OOP,
-            Database and solve 500+ coding problems to become an exceptionally
-            well world-class Programmer.
+          Discover secrets to great food with personalized help and hands-on practice. Lets cook up some tasty fun together and make tasty meals with expert.
           </p>
           <div className="flex gap-4 md:gap-7 justify-center">
-            <button className="btn-accent btn rounded-3xl">Explore Now</button>
-            <button className="btn btn-outline text-white rounded-3xl">Explore Now</button>
+            <button className="bg-c-primary btn rounded-3xl border-none hover:bg-emerald-500 ">Explore Now</button>
+            <button className="btn btn-outline text-white rounded-3xl"> Our Feedback</button>
           </div>
         </div>
     </div>
